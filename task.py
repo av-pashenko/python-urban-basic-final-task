0b5716070a06b2b6c7570e220fff6f413f5eed37
av-pashenko <ar.vi.pashenko@gmail.com>
2024-06-26 16:48:54 +0300

    Первичная загрузка решения финального задания
    Код был отформатирован при помощи black.
    Была выполнена проверка при помощи ruff, все замечания линтера были исправлены, пусть для этого и пришлось отредактировать описания функций, находившиеся в шаблоне кода для задания изначально.

--- task.py
+++ task.py
@@ -1,9 +1,20 @@
+import csv
+
+
 def read_file(filename: str) -> list[dict]:
     """Читает данные из CSV файла и преобразует их в список словарей.
 
     :param filename: Название файла, содержащего данные.
     :return: Список словарей с данными о домах.
     """
+    with open(filename, encoding="utf-8") as f:
+        housing_data = list(csv.DictReader(f))
+    for h in housing_data:
+        h["floor_count"] = int(h["floor_count"])
+        h["population"] = int(h["population"])
+        h["heating_value"] = float(h["heating_value"])
+        h["area_residential"] = float(h["area_residential"])
+    return housing_data
 
 
 def classify_house(floor_count: int) -> str:
@@ -13,8 +24,22 @@ def classify_house(floor_count: int) -> str:
     Возвращает категорию дома в зависимости от количества этажей.
 
     :param floor_count: Количество этажей в доме.
-    :return: Категория дома в виде строки: "Малоэтажный", "Среднеэтажный" или "Многоэтажный".
+    :return: Категория дома в виде строки: "Малоэтажный", "Среднеэтажный" или
+    "Многоэтажный".
     """
+    if not isinstance(floor_count, int):
+        error = "Количество этажей не целое число!"
+        raise TypeError(error)
+    if floor_count <= 0:
+        error = "Количество этажей не положительное!"
+        raise ValueError(error)
+    low_to_med_floors_breakepoint = 5
+    med_to_high_floors_breakepoint = 16
+    if floor_count <= low_to_med_floors_breakepoint:
+        return "Малоэтажный"
+    if floor_count <= med_to_high_floors_breakepoint:
+        return "Среднеэтажный"
+    return "Многоэтажный"
 
 
 def get_classify_houses(houses: list[dict]) -> list[str]:
@@ -23,20 +48,31 @@ def get_classify_houses(houses: list[dict]) -> list[str]:
     :param houses: Список словарей с данными о домах.
     :return: Список категорий домов.
     """
+    return [classify_house(house["floor_count"]) for house in houses]
 
 
 def get_count_house_categories(categories: list[str]) -> dict[str, int]:
-    """
-    Подсчитывает количество домов в каждой категории.
+    """Подсчитывает количество домов в каждой категории.
 
     :param categories: Список категорий домов.
     :return: Словарь с количеством домов в каждой категории.
     """
+    unique_categories = set(categories)
+    return {key: len([c for c in categories if c == key]) for key in unique_categories}
 
 
 def min_area_residential(houses: list[dict]) -> str:
-    """Находит адрес дома с наименьшим средним количеством квадратных метров жилой площади на одного жильца.
+    """Находит адрес дома с наименьшим ср. кол-вом м^2 жилплощади на одного жильца.
 
     :param houses: Список словарей с данными о домах.
-    :return: Адрес дома с наименьшим средним количеством квадратных метров жилой площади на одного жильца.
+    :return: Адрес дома с наименьшим средним количеством
+    квадратных метров жилой площади на одного жильца.
     """
+    min_avg_area = houses[0]["area_residential"] / houses[0]["population"]
+    min_addr = houses[0]["house_address"]
+    for house in houses[1:]:
+        curr_avg_area = house["area_residential"] / house["population"]
+        if curr_avg_area < min_avg_area:
+            min_avg_area = curr_avg_area
+            min_addr = house["house_address"]
+    return min_addr
